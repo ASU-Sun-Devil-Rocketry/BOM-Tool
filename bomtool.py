9976fbe79331ddea8982bbdb7a5e0b7079f04cba
Colton Acosta <cacost12@asu.edu>
2022-01-16 18:43:27 -0700

    wrote method to load pcb data into dictionary

--- bomtool.py
+++ bomtool.py
@@ -1,6 +1,35 @@
 ### BOM Tool -- Python API tool to manage Sun Devil Rocketry 
 ###             PCB BOMs
 
+from pprint import pprint
+
+# PCB class -- contains PCB information extracted from pcbs.txt
+class pcb: 
+
+    # Initializer- pcbdata contains source 
+    #              file name
+    def __init__(self, pcbdata): 
+       
+        # Extract line data from txt file
+        self.file = pcbdata
+        with open(self.file) as sourcefile:
+            lines = sourcefile.readlines()
+
+        # Initialize Dictionary for PCB data
+        self.pcbs = {}
+
+        # Parse Data:
+        for line in lines: 
+           # Check if line starts with "h" for "https" 
+           if(line[0] == 'h'):
+               self.pcbs[self.pcbnum].append(line)
+           else: 
+               self.pcbnum = line[0:5]
+               self.pcbs[self.pcbnum] = [line[6:-1]]
+        pprint(self.pcbs)
+
+
+         
 
 # getOption -- Recursively ask the user for a new selection
 #              until a valid option is entered
@@ -26,5 +55,8 @@ def getOption(choice, options):
 
 
 ### CODE START ### 
+
+# Load PCBs from txt file
+pcbs = pcb("pcbs.txt")
 
 # Display PCB Menu
