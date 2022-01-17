78f32a1d51ff0f7feebdd51c60c34229eb8876c8
Colton Acosta <cacost12@asu.edu>
2022-01-16 18:00:37 -0700

    Added PCB names to txt file

---
+++ bomtool.py
@@ -0,0 +1,30 @@
+### BOM Tool -- Python API tool to manage Sun Devil Rocketry 
+###             PCB BOMs
+
+
+# getOption -- Recursively ask the user for a new selection
+#              until a valid option is entered
+# Inputs: 
+#         choice -- previous selection
+#         options -- number of valid options
+# Output:
+#         choice -- Valid selection
+def getOption(choice, options):
+
+    # User enters a number
+    try:     
+        choice = int(choice)
+        if (choice >= 1 and choice <= options):
+            return(choice)
+        else: 
+            choice = input('Invalid selection. Try again: ')
+            getOption(choice, options)
+    # User enters a non-number
+    except ValueError:
+        choice = input('Invalid selection. Try again: ')
+        getOption(choice, options)
+
+
+### CODE START ### 
+
+# Display PCB Menu
